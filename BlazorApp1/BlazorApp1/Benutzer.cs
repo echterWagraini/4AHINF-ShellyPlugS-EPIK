46319f06754e8faa45d25e9d022c7f35efedd13d
echterWagraini <98883966+echterWagraini@users.noreply.github.com>
2023-03-02 16:17:44 +0100

    Ganzes Projekt

---
+++ BlazorApp1/BlazorApp1/Benutzer.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace BlazorApp1
+{
+    public class Benutzer
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        public string Benutzername { get; set; }
+        public string Email { get; set; }
+        public string Passwort { get; set; }
+        public int MaxStromVerbrauch { get; set; }
+    }
+}
---
+++ BlazorApp1/BlazorApp1/Plug.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorApp1
+{
+    public class Plug
+    { 
+        public bool ison { get; set; }
+        public bool has_timer { get; set; }
+        public float timer_started { get; set; }
+        public float timer_duration { get; set; }
+        public float timer_remaining { get; set; }
+        public bool overpower { get; set; }
+        public string source { get; set; }
+    }
+}
