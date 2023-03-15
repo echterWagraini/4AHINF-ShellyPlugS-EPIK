1f1ca0cac1cd6bfd87199681d40912de1d421042
echterWagraini <98883966+echterWagraini@users.noreply.github.com>
2023-03-15 08:53:36 +0100

    Authentication+new login

---
+++ BlazorApp1/BlazorApp1/Authentication/UserAccountService.cs
@@ -0,0 +1,22 @@
+﻿namespace BlazorApp1.Authentication
+{
+    public class UserAccountService
+    {
+        private List<UserAccount> users;    //Fetch data from DB instead of implementing List (list for test purposes)
+
+        public UserAccountService()
+        {
+            /*users = new List<UserAccount>
+            {
+                new UserAccount{UserName="admin",Password="admin",Role="Admininstrator"},
+                new UserAccount {UserName="user",Password="user",Role="User"}
+            };*/
+
+        }
+
+        public UserAccount? GetByUserName(string userName)
+        {
+            return users.FirstOrDefault(x => x.UserName == userName); ;
+        }
+    }
+}
